93edae31e45383c916c2c2357e9cc522bc4fae01
ly921002 <92194283+ly921002@users.noreply.github.com>
2025-10-28 13:52:51 +0800

    Update index_argo.js

--- index_argo.js
+++ index_argo.js
@@ -813,7 +813,7 @@ async function extractTempDomains() {
     console.error('Error extracting temporary domains:', error);
     await generateLinks('error.trycloudflare.com');
   }
-
+}
 
   // 生成 list 和 sub 信息
   async function generateLinks(argoDomain) {
@@ -847,7 +847,7 @@ async function extractTempDomains() {
       }, 2000);
     });
   }
-}
+
 
 // 自动上传节点或订阅
 async function uploadNodes() {
